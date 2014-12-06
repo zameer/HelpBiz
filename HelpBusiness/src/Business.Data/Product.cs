b897057c86fd256460c54c9a80c3b39f93b405d2
Ahamed Zameer <ahamedzameer@hotmail.com>
2014-12-06 17:29:07 +0530

    product class created

---
+++ HelpBusiness/src/Business.Data/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Business.Data
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
